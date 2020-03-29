fb7dd07d98438db1ed071d083b02a186cec76f29
ronmnm <spellbound.2808@gmail.com>
2020-03-29 18:00:16 +0300

    manage tab layout

--- src/components/manage/Manage.js
+++ src/components/manage/Manage.js
@@ -23,11 +23,12 @@ export default props => {
   props.setWinddown();
   return (
     <div className="my_manage">
+      {/* Staker Left Side */}
       <div className="staker_manage big_item">
-        <h5 className="address_title">Staker Account</h5>
-        <h4 className="address_eth">
+        <h4 className="address_title">Staker Account</h4>
+        <h2 className="address_eth">
           {staker.slice(0, 6)}...{staker.slice(38, 42)}
-        </h4>
+        </h2>
 
         <div className="staker_content">
           <p>
@@ -42,21 +43,24 @@ export default props => {
         </div>
         <div className="staker_buttons">
           <div className="restake">
-            <h5 className="restake_text">Re-Stake</h5>
+            <h4 className="restake_text">Re-Stake<span className="restake_status">Unlocked</span></h4>
             <button onClick={setWinddownClick}>True</button>
             <button>False</button>
           </div>
           <div className="winddown">
-            <h5 className="winddown_text">Wind-Down</h5>
+            <h4 className="winddown_text">Wind-Down</h4>
+            <button onClick={setWinddownClick}>True</button>
+            <button>False</button>
           </div>
         </div>
       </div>
-      <div></div>
+
+      {/* Worker Rigth Side */}
       <div className="worker_manage big_item">
-        <h5 className="address_title">Worker Account</h5>
-        <h4 className="address_eth">
+        <h4 className="address_title">Worker Account</h4>
+        <h2 className="address_eth">
           {worker.slice(0, 6)}...{worker.slice(38, 42)}
-        </h4>
+        </h2>
 
         <div className="worker_content">
           <p>
@@ -76,33 +80,70 @@ export default props => {
           <NavLink className="my_nav_item" to="/manage/action-history">
             Action History
           </NavLink>
-
-          <Route path='/manage' exact component={SubstakeList} />
-          <Route path='/manage/substake-list' component={SubstakeList} />
-          <Route path='/manage/action-history' component={ActionHistory} />
-
-          
         </div>
+
+        <Route path="/manage" exact component={SubstakeList} />
+        <Route path="/manage/substake-list" component={SubstakeList} />
+        <Route path="/manage/action-history" component={ActionHistory} />
       </div>
-      
     </div>
   );
 };
 
+
+
 const SubstakeList = () => {
   return (
-    <div className="list_item">
-      <div>Stake</div>
-      <div>Stake</div>
-      <div>Stake</div>
-      <div>Stake</div>
-      <div>Stake</div>
+    <div>
+      <div className="substake_items_titles">
+        <span>Substake</span>
+        <span>Amount</span>
+        <span>Duration</span>
+        <span>Created</span>
+        <span>Ends</span>
+        <span>Modify</span>
+      </div>
+      <div className="list_items">
+        <div className="substake_item">
+          <div>
+            <span>№0</span>
+            <span>STAKE</span>
+          </div>
+          <div>
+            <span>956,000.55</span>
+            <span>NU</span>
+          </div>
+          <div>
+            <span>365</span>
+            <span>DAYS</span>
+          </div>
+          <div>
+            <span>21 FEB</span>
+            <span>2020</span>
+          </div>
+          <div>
+            <span>21 FEB</span>
+            <span>2021</span>
+          </div>
+          <div>
+            <span>№0</span>
+            <span>STAKE</span>
+          </div>
+          
+        </div>
+        <div className="substake_item">
+          <div>Stake</div>
+          <div>Stake</div>
+          <div>Stake</div>
+          <div>Stake</div>
+          <div>Stake</div>
+          <div>Stake</div>
+        </div>
+      </div>
     </div>
   );
 };
 
 const ActionHistory = () => {
-  return(
-    <div>Action History</div>
-  )
-}
+  return <div>Action History</div>;
+};
