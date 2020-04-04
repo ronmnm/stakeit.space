e5ee6d84f14212d898f28a224fdd7bb91aac7d63
ronmnm <spellbound.2808@gmail.com>
2020-04-04 14:31:12 +0300

    stake service done

--- src/services/stake-service.js
+++ src/services/stake-service.js
@@ -2,62 +2,42 @@ import Web3 from "web3";
 import { instanceToken } from "../ethereum/instances/instances";
 
 const web3 = new Web3(window.ethereum);
-const dispatcher = '0xAB51fBDd4Faf6c691884B3A9b475E34E2092aE81'
+const DISPATCHER = "0xAB51fBDd4Faf6c691884B3A9b475E34E2092aE81";
 
 export default class StakeService {
+
+   _hexToBytes(hex) {
+      hex = hex.toString(16);
+
+      hex = hex.replace(/^0x/i, "");
+      hex = hex.length % 2 ? "0" + hex : hex;
+
+      let bytes = [];
+      for (let c = 0; c < hex.length; c += 2) {
+         bytes.push(parseInt(hex.substr(c, 2), 16));
+      }
+
+      return bytes;
+   }
+
    getApproveAndCall() {
-     
-
       const approveAndCall = async (inputAmount, inputDuration) => {
-         const accounts = await web3.eth.getAccounts()
+         const accounts = await web3.eth.getAccounts();
 
+         const amount = web3.utils.toWei(inputAmount, "ether");
 
-         const hexToBytesFixed = (hex) => {
-            hex = hex.toString(16);
-        
-         
-        
-            hex = hex.replace(/^0x/i, '');
-            hex = hex.length % 2 ? '0' + hex : hex;
-        
-            let bytes = [];
-            for (let c = 0; c < hex.length; c += 2) {
-                bytes.push(parseInt(hex.substr(c, 2), 16));
-            }
-        
-            return bytes;
-          }
-
-
-
-         const hex = web3.utils.numberToHex('234')
-         console.log(hex);
-         const duration = hexToBytesFixed('0x000000ea')
+         const hex = web3.utils.numberToHex(inputDuration);
+         const duration = this._hexToBytes(hex);
          console.log(duration);
-         console.log('duration', typeof inputDuration);
-         // debugger;
-         let arr = [4, 50]
+
          try {
-            await instanceToken.methods.approveAndCall(dispatcher, 15000, duration)
-            
-               .send({from: accounts[0]})
-            
+            await instanceToken.methods
+               .approveAndCall(DISPATCHER, amount, duration)
+               .send({ from: accounts[0] });
          } catch (error) {
             console.log(error);
          }
-
-
-         
-         //  hexToBytesFixed(hex)
-
-
-
-
-         
-         
-         
-         
-      }
+      };
       return approveAndCall;
    }
 }
