fc1bbae5722b0f5253ff6a8b9f4ebbeb2a02fea8
ronmnm <spellbound.2808@gmail.com>
2020-04-16 19:08:29 +0300

    status button refactoring

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,67 +1,36 @@
-import React from "react";
+import React from 'react';
 // import Nav from "./Nav";
-import Address from "./address/Address";
-import "./Header.css";
-import LoadingAddress from "./address/loading-address/Address-loading";
-import ConnectWallet from "./address/connect-a-wallet/connect-a-wallet";
-import WrongNetwork from "./address/wrong-network/wrong-network";
-import InstallMetamask from "./address/install-metamask/install-metamask";
+import Address from './address/Address';
+import './Header.css';
 
-import { NavLink } from "react-router-dom";
-// import "./Nav.css";
+import { NavLink } from 'react-router-dom';
+
 
 export default class Header extends React.Component {
-  // state = {
-  //   connected: null
-  // }
 
-  render() {
-    let addressButton;
+   render() {
 
-    switch (this.props.buttonStatus) {
-      case "loading":
-        addressButton = <LoadingAddress />;
-        break;
-      case "wrong":
-        addressButton = <WrongNetwork />;
-        break;
-      case "ok":
-        addressButton = <Address />;
-        break;
-      case "connect":
-        addressButton = <ConnectWallet onClick={this.props.onClick} />;
-        break;
-      case "install":
-        addressButton = <InstallMetamask />;
-        break;
-      default:
-        console.log("default case switch");
-    }
-
-    return (
-      <div className="my_header">
-        <div className="logo">
-          <span>stakeit.space</span>
-        </div>
-        <div className="my_nav">
-          <NavLink className="my_nav_item" to="/stake">
-            Stake
-          </NavLink>
-          <NavLink className="my_nav_item" to="/manage">
-            Manage
-          </NavLink>
-          {/* <NavLink className="my_nav_item disable" to="/rewards">
+      return (
+         <div className="my_header">
+            <div className="logo">
+               <span>stakeit.space</span>
+            </div>
+            <div className="my_nav">
+               <NavLink className="my_nav_item" to="/stake">
+                  Stake
+               </NavLink>
+               <NavLink className="my_nav_item" to="/manage">
+                  Manage
+               </NavLink>
+               {/* <NavLink className="my_nav_item disable" to="/rewards">
             Rewards
           </NavLink> */}
-          <NavLink className="my_nav_item" to="/worklock">
-            Worklock
-          </NavLink>
-        </div>
-
-        {/* <Address account={this.props.account} /> */}
-        {/* {addressButton} */}
-        <div className="my_address">{addressButton}</div>
-      </div>
-    );
-  }
+               <NavLink className="my_nav_item" to="/worklock">
+                  Worklock
+               </NavLink>
+            </div>
+            <div className="my_address"><Address /></div>
+         </div>
+      );
+   }
 }
--- src/components/header/address/Address.js
+++ src/components/header/address/Address.js
@@ -1,31 +1,130 @@
 import React from 'react';
-import s from './Address.module.css';
+import styled from 'styled-components';
 import Blockies from 'react-blockies';
 import { connect } from 'react-redux';
+import { colors } from '../../../constants/colors';
+import RoundSpinner from '../../loader/7.svg';
 
-const Address = ({ account }) => {
-   return (
-      <React.Fragment>
-         {/* <span>{this.props.network}</span> */}
-         <div className={s.my_address_item}>
-            <div className={s.my_ident_icon}>
-               <Blockies 
-                  seed={account} 
-                  size={7} 
-                  scale={3} 
-                  color="#B692BE" 
-                  spotColor="#18335E" />
-            </div>
-            <div className={s.my_address_text}>
-               {account.slice(0, 6)}...{account.slice(38, 42)}
-            </div>
-         </div>
-      </React.Fragment>
-   );
+const InstallButton = styled.div`
+   width: 140px;
+   height: 30px;
+   border-radius: 8px;
+   background-color: #333;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   transition: 0.3s;
+   &:hover {
+      background-color: #444;
+      cursor: pointer;
+   }
+   span {
+      font-size: 13px;
+      font-weight: 500;
+      letter-spacing: 0.5px;
+   }
+`;
+
+const LoadingButton = styled(InstallButton)`
+   &:hover {
+      cursor: default;
+      background-color: #333;
+   }
+   img {
+      margin: auto;
+   }
+`;
+
+const AddressButton = styled(InstallButton)`
+   justify-content: flex-start;
+   .ident_icon {
+      display: flex;
+      align-items: center;
+      height: 30px;
+      width: 30px;
+      border-radius: 7px;
+      canvas {
+         border-radius: 5px;
+         margin: auto;
+      }
+   }
+   span {
+      letter-spacing: 0.5px;
+      display: flex;
+      font-size: 13px;
+      color: ${colors.whiteText};
+      justify-content: flex-end;
+      margin: 0 auto;
+   }
+`;
+
+const ConnectButton = styled(InstallButton)`
+   background-color: ${colors.blue};
+   /* border: 1px solid ${colors.blue}; */
+   span {
+      color: ${colors.whiteText};
+      transition: .2s;
+   }
+   &:hover {
+      background-color: ${colors.hoverBlue};
+   }
+`;
+
+const WrongButton = styled(InstallButton)`
+   background-color: rgb(48, 7, 0);
+   border: 1px solid #cf3903;
+   &:hover {
+      cursor: default;
+      background-color: rgb(48, 7, 0);
+   }
+   span {
+      color: #ff5416;
+   }
+`;
+
+const Address = ({ account, status, onClick }) => {
+   // let content;
+   switch (status) {
+      case 'OK':
+         return (
+            <AddressButton>
+               <div className="ident_icon">
+                  <Blockies seed={account} size={7} scale={3} color="#B692BE" spotColor="#18335E" />
+               </div>
+               <span>
+                  {account.slice(0, 6)}...{account.slice(38, 42)}
+               </span>
+            </AddressButton>
+         );
+      case 'WRONG':
+         return (
+            <WrongButton>
+               <span>Wrong Network</span>
+            </WrongButton>
+         );
+      case 'LOADING':
+         return (
+            <LoadingButton>
+               <img src={RoundSpinner} alt="load" />
+            </LoadingButton>
+         );
+      case 'INSTALL':
+         return (
+            <InstallButton>
+               <span>Install Metamask</span>
+            </InstallButton>
+         );
+      case 'CONNECT':
+         return (
+            <ConnectButton onClick={onClick}>
+               <span>Connect a wallet</span>
+            </ConnectButton>
+         );
+      default:
+         return <div>hi</div>
+   }
 };
 
-const mapStateToProps = (state) => {
-   
-   return {account: state.address}
-}
+const mapStateToProps = ({address, status}) => ({ account: address.address, status: status.status, onClick: status.onConnectClick });
+
 export default connect(mapStateToProps)(Address);
