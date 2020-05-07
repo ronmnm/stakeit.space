7a82c86236bbbfdbb00fd73531d1f99876b2463f
ronmnm <spellbound.2808@gmail.com>
2020-05-07 23:29:02 +0300

    worker field fixes + withdraw rewards section

---
+++ src/components/manage/worker/withdrawRewards.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+
+const RewardsSection = styled.form`
+   .rewards_title {
+      display: grid;
+      justify-content: space-between;
+      grid-auto-flow: column;
+      margin-top: 35px;
+      margin-bottom: 5px;
+      padding-bottom: 11px;
+      border-bottom: 1px solid ${({theme}) => theme.independenceDark};
+   }
+   .rewards_items {
+      padding-bottom: 5px;
+   }
+`;
+const RewardItemWrapper = styled.div`
+   display: grid;
+   grid-auto-flow: column;
+   grid-template-columns: 110px 1fr;
+   margin: 0;
+   color: ${({ theme }) => theme.textSecondary};
+   b {
+      color: ${({ theme }) => theme.textPrimary};
+   }
+   div {
+      font-size: 13px;
+      height: 30px;
+      line-height: 30px;
+   }
+   div:last-child {
+      display: grid;
+      grid-auto-flow: column;
+      justify-content: flex-end;
+      b {
+         font-weight: 500;
+         letter-spacing: .5px;
+         span {
+            color: grey;
+         }
+      }
+   }
+`;
+
+const WithdrawButton = styled.span`
+   width: 28px;
+   height: 28px;
+   border-radius: 8px;
+   margin: 0 16px 0 27px;
+   padding: 1px 6px 8px 6px;
+   svg {
+      fill: ${({ theme }) => theme.textSecondary};
+   }
+   &:hover {
+      cursor: pointer;
+      background-color: ${({ theme }) => theme.backgroundPaleHover};
+      svg {
+         fill: ${({ theme }) => theme.textPrimary};
+      }
+   }
+`;
+
+const WithdrawRewards = ({ stakingRewards, policyFee }) => {
+   return (
+      <RewardsSection>
+         <div className="rewards_title">
+            <span>Rewards</span>
+            <span>Withdraw</span>
+         </div>
+         <div className="rewards_items">
+            <RewardItem text="Staking Rewards:" value={stakingRewards.toLocaleString('en-Us')} label="NU" />
+            <RewardItem text="Policy Fee:" value={policyFee} label="ETH" />
+         </div>
+      </RewardsSection>
+   );
+};
+
+const RewardItem = ({ text, value, label }) => {
+   return (
+      <RewardItemWrapper>
+         <div>{text}</div>
+         <div>
+            <b>{value}<span> {label}</span></b>
+            <WithdrawButton>
+               <svg
+                  id="bold"
+                  enableBackground="new 0 0 24 24"
+                  viewBox="0 0 24 24"
+               >
+                  <path d="m13.25 18h-2.5c-.689 0-1.25-.561-1.25-1.25v-5.75h-2.75c-.657 0-.998-.791-.543-1.268l5.25-5.5c.283-.296.803-.296 1.086 0l5.25 5.5c.455.477.114 1.268-.543 1.268h-2.75v5.75c0 .689-.561 1.25-1.25 1.25z" />
+                  <path d="m21 24h-18c-1.654 0-3-1.346-3-3v-5c0-1.654 1.346-3 3-3h1.5c.552 0 1 .448 1 1s-.448 1-1 1h-1.5c-.551 0-1 .449-1 1v5c0 .551.449 1 1 1h18c.551 0 1-.449 1-1v-5c0-.551-.449-1-1-1h-1.5c-.552 0-1-.448-1-1s.448-1 1-1h1.5c1.654 0 3 1.346 3 3v5c0 1.654-1.346 3-3 3z" />
+               </svg>
+            </WithdrawButton>
+         </div>
+      </RewardItemWrapper>
+   );
+};
+
+const mapDispatchToProps = ({ user }) => ({
+   stakingRewards: user.manage.stakerNuUnlocked,
+   policyFee: user.manage.policyFee,
+});
+export default connect(mapDispatchToProps)(WithdrawRewards);
--- src/components/manage/worker/worker.js
+++ src/components/manage/worker/worker.js
@@ -1,29 +1,33 @@
-import React, { useRef, useState, useEffect } from "react";
-import styled from "styled-components";
-import utils from "web3-utils";
-import ReactGA from "react-ga";
-import s from "./worker.module.css";
-import Icon from "../link-icon/link-icon";
-import Modal from "../../worklock/modal/modal";
-import { ControlButton } from "../manage-bottom/substakes/substakes";
-import RoundSpinner from "../../loader/7.svg";
+import React, { useRef, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import utils from 'web3-utils';
+import ReactGA from 'react-ga';
+import Modal from '../../worklock/modal/modal';
+import { ControlButton } from '../manage-bottom/substakes/substakes';
+import RoundSpinner from '../../loader/7.svg';
+import EthAccountContainer from '../eth-accont-container';
+import { connect } from 'react-redux';
+
+import WithdrawRewards from './withdrawRewards';
 
 const WorkerButton = styled.span`
-   background-color: #404040;
+   background-color: #333811;
+   border: 1px solid #daefb3;
    padding: 0 24px;
-   height: 100%;
+   height: 30px;
+   line-height: 29px;
+   font-size: 14px;
    display: block;
    justify-self: flex-end;
    font-size: 13px;
    border-radius: 8px;
    text-align: center;
    font-weight: 500;
-   transition: 0.2s;
-   color: #cccccc;
+   color: #daefb3;
    :hover {
       cursor: pointer;
-      background-color: #4b4b4b;
-      color: #ffffff;
+      /* background-color: #4b4b4b; */
+      color: #E2FF18;
    }
    :active {
       background-color: #444444;
@@ -76,7 +80,7 @@ export const ModalContent = styled.div`
       margin-bottom: 60px;
       letter-spacing: 0.4px;
       font-family: Lato, sans-serif;
-      border: 1px solid ${({ isValid }) => (isValid ? "#4b4b4b" : "tomato")};
+      border: 1px solid ${({ isValid }) => (isValid ? '#4b4b4b' : 'tomato')};
       &:focus {
          outline: none;
       }
@@ -87,12 +91,12 @@ export const ModalContent = styled.div`
          opacity: 0;
       }
    }
-   input[type="number"]::-webkit-inner-spin-button,
-   input[type="number"]::-webkit-outer-spin-button {
+   input[type='number']::-webkit-inner-spin-button,
+   input[type='number']::-webkit-outer-spin-button {
       -webkit-appearance: none;
       margin: 0;
    }
-   input[type="number"] {
+   input[type='number'] {
       -moz-appearance: textfield;
    }
    .button_wrapper {
@@ -102,23 +106,48 @@ export const ModalContent = styled.div`
    }
 `;
 
+const WorkerWrapper = styled.div`
+   grid-area: worker;
+   .worker_content {
+      margin: 10px 0;
+      padding-top: 5px;
+      padding-bottom: 5px;
+      /* border-bottom: 1px solid #333; */
+      border-top: 1px solid ${({ theme }) => theme.independenceDark};
+      p {
+         display: grid;
+         grid-auto-flow: column;
+         justify-content: space-between;
+         margin: 7px 0;
+         color: rgb(119, 119, 119);
+         font-size: 13px;
+         span {
+            color: grey;
+            b {
+               color: #eee;
+               font-weight: 500;
+               letter-spacing: 0.8px;
+            }
+         }
+      }
+   }
+`;
+
 const Worker = ({ worker, workerEthBal, setWorker, confirmedPeriod1 }) => {
-   const [inputAddress, setInputAddress] = useState("");
+   const [inputAddress, setInputAddress] = useState('');
    const [isValid, setIsValid] = useState(true);
    const [isLoading, setIsLoading] = useState(false);
+   const [copied, setCopied] = useState(false);
 
-   let workerAddress;
    let buttonContent;
-   if (worker === "0x0000000000000000000000000000000000000000") {
-      workerAddress = <React.Fragment>Worker not set</React.Fragment>;
-      buttonContent = "Set worker";
+   if (worker === '0x0000000000000000000000000000000000000000') {
+      buttonContent = 'Set worker';
    } else {
-      workerAddress = `${worker.slice(0, 6)}...${worker.slice(38, 42)}`;
-      buttonContent = "Change worker";
+      buttonContent = 'Change worker';
    }
 
    useEffect(() => {
-      if (inputAddress !== "") {
+      if (inputAddress !== '') {
          setIsValid(utils.isAddress(inputAddress));
       } else {
          setIsValid(true);
@@ -127,38 +156,39 @@ const Worker = ({ worker, workerEthBal, setWorker, confirmedPeriod1 }) => {
 
    const modalRef = useRef();
 
-   const openModal = (e) => {
+   const openModal = e => {
       e.preventDefault();
       modalRef.current.openMod();
    };
    const closeModal = () => {
       modalRef.current.closeMod();
-      setInputAddress("");
+      setInputAddress('');
       setIsValid(true);
    };
 
    const handleClick = async () => {
       setIsLoading(true);
       ReactGA.event({
-         category: "Manage tab",
-         action: "Open Set Worker Modal",
-         label: "manage_tab_label"
+         category: 'Manage tab',
+         action: 'Open Set Worker Modal',
+         label: 'manage_tab_label',
       });
       await setWorker(inputAddress)
          .then(() => setIsLoading(false))
          .catch(() => setIsLoading(false));
-      setInputAddress("");
+      setInputAddress('');
+   };
+
+   const onAddrClick = () => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1000);
    };
 
    return (
-      <div className="worker_manage">
-         <h4 className={s.address_title}>Worker Account</h4>
-         <h2 className={s.address_eth}>
-            {workerAddress}
-            <Icon address={worker} />
+      <WorkerWrapper>
+         <EthAccountContainer copied={copied} onAddrClick={onAddrClick} address={worker} title="Worker Account">
             <WorkerButton onClick={openModal}>{buttonContent}</WorkerButton>
-         </h2>
-
+         </EthAccountContainer>
          <div className="worker_content">
             <p>
                Ether Balance:
@@ -173,92 +203,40 @@ const Worker = ({ worker, workerEthBal, setWorker, confirmedPeriod1 }) => {
                </span>
             </p>
          </div>
-
+         <WithdrawRewards />
          <div>
             <Modal ref={modalRef}>
                <ModalContent isValid={isValid}>
                   <span>Bond a worker to a staker</span>
                   <div>
                      {/* <b>Bond a worker to a staker</b> */}
-                     <b>
-                        Note: The Worker cannot be changed for a minimum of 2
-                        periods once set.
-                     </b>
+                     <b>Note: The Worker cannot be changed for a minimum of 2 periods once set.</b>
                      <label htmlFor="">Enter ETH address:</label>
-                     <input
-                        placeholder="0x..."
-                        value={inputAddress}
-                        onChange={(e) => setInputAddress(e.target.value)}
-                     />
+                     <input placeholder="0x..." value={inputAddress} onChange={e => setInputAddress(e.target.value)} />
                   </div>
                   <div className="button_wrapper">
-                     <ControlButton
-                        onClick={closeModal}
-                        background="#444"
-                        background_hover="#484848">
+                     <ControlButton onClick={closeModal} background="#444" background_hover="#484848">
                         Cancel
                      </ControlButton>
-                     <ControlButton
-                        blue
-                        onClick={handleClick}
-                        background="#0077ff"
-                        background_hover="#1683ff">
+                     <ControlButton blue onClick={handleClick} background="#0077ff" background_hover="#1683ff">
                         {isLoading ? (
-                           <img
-                              style={{ marginTop: "10px" }}
-                              src={RoundSpinner}
-                              alt="React Logo"
-                           />
+                           <img style={{ marginTop: '10px' }} src={RoundSpinner} alt="React Logo" />
                         ) : (
-                           "Confirm"
+                           'Confirm'
                         )}
                      </ControlButton>
                   </div>
                </ModalContent>
             </Modal>
          </div>
-      </div>
+      </WorkerWrapper>
    );
 };
 
-export default Worker;
-
-// class ChangeWorkerField extends React.Component {
-//    constructor(props) {
-//       super(props);
-//       this.state = {
-//          address: "",
-//       };
-
-//       this.handleChange = this.handleChange.bind(this);
-//       this.handleSubmit = this.handleSubmit.bind(this);
-//    }
-
-//    handleChange(event) {
-//       this.setState({ address: event.target.value });
-//    }
-
-//    handleSubmit(event) {
-//       event.preventDefault();
-//       this.props.setWorker(this.state.address);
-//    }
-
-//    render() {
-//       return (
-//          <div>
-//             <p>Set New Worker</p>
-
-//             <form onSubmit={this.handleSubmit} className={s.worker_buttons}>
-//                <button className={s.change_wrkr_button}>Confirm</button>
-//                <input
-//                   placeholder="Enter new ETH address"
-//                   className={s.change_wrkr_input}
-//                   type="text"
-//                   value={this.state.address}
-//                   onChange={this.handleChange}
-//                />
-//             </form>
-//          </div>
-//       );
-//    }
-// }
+const mapStateToProps = ({ user }) => ({
+   worker: user.manage.worker,
+   workerEthBal: user.manage.workerEthBal,
+   setWorker: user.manage.setWorker,
+   confirmedPeriod1: user.manage.confirmedPeriod1,
+});
+export default connect(mapStateToProps)(Worker);
