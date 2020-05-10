6b00cd726373d706094825b8de6630c3e33f699e
ronmnm <spellbound.2808@gmail.com>
2020-05-10 16:02:22 +0300

    stake service refactoring + test

--- src/services/stake-service.js
+++ src/services/stake-service.js
@@ -1,43 +1,22 @@
-import Web3 from "web3";
-import { Token } from "../ethereum/instances/instances";
+import Web3 from 'web3';
+import { Token } from '../ethereum/instances/instances';
+import { hexToBytes } from '../utils/utils';
 
+const DISPATCHER_ADDRESS = '0xAB51fBDd4Faf6c691884B3A9b475E34E2092aE81';
 const web3 = new Web3(window.ethereum);
-const DISPATCHER = "0xAB51fBDd4Faf6c691884B3A9b475E34E2092aE81";
 
-export default class StakeService {
+export const addNewStake = async (inputAmount, inputDuration) => {
+   const account = window.ethereum.selectedAddress
+   const amount = web3.utils.toWei(String(inputAmount), 'ether');
+   const hex = web3.utils.numberToHex(inputDuration);
+   // const duration = web3.utils.hexToBytes(hex)
+   const duration = hexToBytes(hex);
 
-   _hexToBytes(hex) {
-      hex = hex.toString(16);
-
-      hex = hex.replace(/^0x/i, "");
-      hex = hex.length % 2 ? "0" + hex : hex;
-
-      let bytes = [];
-      for (let c = 0; c < hex.length; c += 2) {
-         bytes.push(parseInt(hex.substr(c, 2), 16));
-      }
-
-      return bytes;
+   try {
+      return await Token.methods
+         .approveAndCall(DISPATCHER_ADDRESS, amount, duration)
+         .send({ from: account });
+   } catch (error) {
+      console.log(error);
    }
-
-   getApproveAndCall() {
-
-      const approveAndCall = async (inputAmount, inputDuration) => {
-
-         const accounts = await web3.eth.getAccounts();
-         const amount = web3.utils.toWei(inputAmount, "ether");
-         const hex = web3.utils.numberToHex(inputDuration);
-         const duration = this._hexToBytes(hex);
-
-
-         try {
-            await Token.methods
-               .approveAndCall(DISPATCHER, amount, duration)
-               .send({ from: accounts[0] });
-         } catch (error) {
-            console.log(error);
-         }
-      };
-      return approveAndCall;
-   }
-}
+};
---
+++ src/services/stake-service.test.js
@@ -0,0 +1,38 @@
+import { addNewStake } from './stake-service';
+
+/**
+ * mocking the Token contract instance
+ * mock just returns data that passed to function:
+ * approveAndCall(address _spender, uint256 _value, bytes calldata _extraData)
+ */
+jest.mock('../ethereum/instances/instances.js', () => ({
+   Token: {
+      methods: {
+         approveAndCall: (DISPATCHER_ADDRESS, amount, duration) => ({
+            send: obj => ({
+               DISPATCHER_ADDRESS,
+               amount,
+               duration,
+            }),
+         }),
+      },
+   },
+}));
+
+describe('Stake service. Testing addNewStake function', () => {
+   let result;
+   beforeEach(async () => {
+      global.ethereum = jest.fn();
+      result = await addNewStake(45000.5, 365);
+   });
+
+   it('should return correct dispatcher eth address', () => {
+      expect(result.DISPATCHER_ADDRESS).toBe('0xAB51fBDd4Faf6c691884B3A9b475E34E2092aE81');
+   });
+   it('should correctly convert NU (inputAmount) into NuNits', () => {
+      expect(result.amount).toBe('45000500000000000000000');
+   });
+   it('should correctly convert days (inputDuration) into array of bytes', () => {
+      expect(result.duration).toEqual([1, 109]);
+   });
+});
