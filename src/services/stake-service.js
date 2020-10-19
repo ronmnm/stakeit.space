9341728fbab2c900f62c211c410614853e1d6c4c
ronmnm <spellbound.2808@gmail.com>
2020-10-19 23:54:44 +0300

    remove console log

--- src/services/stake-service.js
+++ src/services/stake-service.js
@@ -1,11 +1,8 @@
 import Web3 from "web3"
 import { Token, Escrow } from "../ethereum/instances/instances"
-import { hexToBytes } from "../utils/utils"
+
 
 const web3 = new Web3(window.ethereum)
-const hexTest = web3.utils.numberToHex("30")
-console.log("hex", hexTest)
-console.log("hexToBytes", web3.utils.hexToBytes(hexTest))
 
 export const addNewStake = async (inputAmount, inputDuration) => {
   const account = window.ethereum.selectedAddress
